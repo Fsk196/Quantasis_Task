4847dfe46bc78607c979290072c600d38a2af6c1
Fsk196 <kshoeb196@gmail.com>
2024-09-04 00:15:05 +0530

    Images routings

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Circle } from "./../assets/Circle.png";
 export { default as Rectangle } from "./../assets/rectangle.png";
 export { default as Dots } from "./../assets/Dots.png";
-export { default as Wave } from "./../assets/Wave.png";
+export { default as Wave } from "./../assets/wave.png";
